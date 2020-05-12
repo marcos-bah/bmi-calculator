e5f3bb5ba5af575f6627ace4661d7b8c9e58cacd
marcos-bah <marcos.barbosa@unifei.edu.br>
2020-05-11 22:30:42 -0300

    add gif doc

--- lib/blocs/imc.bloc.dart
+++ lib/blocs/imc.bloc.dart
@@ -19,7 +19,7 @@ class ImcBloc {
       result =
           "Abaixo do Peso\n Peso ideal entre ${min.toStringAsPrecision(4)} e ${max.toStringAsPrecision(4)}";
     } else if (imc >= 18.6 && imc <= 24.9) {
-      result = "Peso Ideal (${imc.toStringAsPrecision(2)}";
+      result = "Peso Ideal";
     } else if (imc <= 29.9) {
       double min = h * h * 24.9;
       double max = h * h * 29.9;
